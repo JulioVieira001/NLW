17ec0397f6c099f1de4cd6d7bcb7055db91cae43
Julio Vieira <juliocesarvieira19@gmail.com>
2025-07-08 10:48:32 -0300

    Dia 2

---
+++ index.js
@@ -0,0 +1,12 @@
+const apiKeyInput = document.gerElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionId = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('Form')
+
+const enviarFormulario = (event) =>{
+
+
+}
+form.addEventListener('submit', enviarFormulario)
